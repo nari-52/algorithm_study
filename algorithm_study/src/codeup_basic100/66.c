d8b0fd0b71d9675d6ed7f4c814cfcd0c7e2d4e4f
nari-52 <60787793+nari-52@users.noreply.github.com>
2020-09-12 13:56:48 +0900

    200912

---
+++ algorithm_study/src/codeup_basic100/66.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int i, a[3];
+  scanf("%d %d %d", &a[0], &a[1], &a[2]);
+
+  for(i=0; i<3; i++) {
+    if(a[i] % 2 == 0) {
+      printf("even\n");
+    }
+    else {
+      printf("odd\n");
+    }
+  }
+  return 0;
+}
---
+++ algorithm_study/src/codeup_basic100/68.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+  int a;
+  scanf("%d", &a);
+
+  if (a >= 90) {
+    printf("A\n");
+  } else if ( a >= 70) {
+    printf("B\n");
+  } else if ( a >= 40) {
+    printf("C\n");
+  } else printf("D\n");
+
+  return 0;
+}
