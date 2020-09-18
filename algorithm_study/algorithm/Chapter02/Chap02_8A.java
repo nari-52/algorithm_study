c907230b180432a0e37aa7ef05e979ce092733d0
nari-52 <60787793+nari-52@users.noreply.github.com>
2020-09-18 22:09:06 +0900

    200918

--- algorithm_study/algorithm/Chapter02/Chap02_8A.java
+++ algorithm_study/algorithm/Chapter02/Chap02_8A.java
@@ -1,10 +1,19 @@
 package Chapter02;
 
 public class Chap02_8A {
+	// 입력받은 10진수를 2 ~ 36 진수로 기수 변환하여 나타내기
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	static int cardConvR(int x, int r, char[] d) {
+		int digits = 0; // 변환수의 자릿수를 나타내기 위한 변수
+		String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		
+		do {
+			d[digits++] = dchar.charAt(x % r);
+			x /= r;
+		} while (x != 0);
+		
+		return digits;
+		
 	}
 
 }
---
+++ algorithm_study/algorithm/Chapter02_Prac/Q6.java
@@ -0,0 +1,59 @@
+package Chapter02_Prac;
+
+import java.util.Scanner;
+
+public class Q6 {
+	
+	// 입력받은 10진수를 2 ~ 36 진수로 기수 변환하여 나타내기
+	static int cardConv(int x, int r, char[] d) {
+		int digits = 0; // 변환수의 자릿수를 나타내기 위한 변수
+		String dchar = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		
+		do {
+			d[digits++] = dchar.charAt(x % r);
+			x /= r;
+		} while (x != 0);
+		
+		for(int i=0; i<digits/2; i++) {
+			char temp = d[i];
+			d[i] = d[digits-i-1];
+			d[digits-i-1] = temp;
+			
+		}
+		return digits;
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		int no, cd, dno, retry;
+		char[] cno = new char[32];
+		
+		System.out.println("10진수를 기수 변환합니다.");
+		do {
+			do {
+				System.out.print("변환하는 음이 아닌 정수 : ");
+				no = stdIn.nextInt();
+			} while (no < 0);
+			
+			do {
+				System.out.print("어떤 진수로 변환할까요? (2 ~ 36) : ");
+				cd = stdIn.nextInt();
+			} while (cd < 2 || cd > 36);
+			
+			dno = cardConv(no, cd, cno);
+			
+			System.out.print(cd + "진수로는 : ");
+			for(int i= 0; i<dno; i++) {
+				System.out.print(cno[i]);
+			}
+			System.out.println("\n------------------");
+			
+			System.out.print("한번 더 할까요? (1. 예 / 2. 아니오) : ");
+			retry = stdIn.nextInt();
+		} while (retry == 1);
+		
+
+	}
+
+}
